2802566a260f96e7f5d067afecd9491b19a4a08b
ShivangiPandey <72519007+Shivangi2012-d@users.noreply.github.com>
2022-08-09 20:07:45 +0530

    Time: 235 ms (65.25%), Space: 18.3 MB (42.88%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        /*
-        Note:
-        Each of the array element will not exceed 100.
-        The array size will not exceed 200.
-        
-        we want to check if we can make up sum/2 by some elements,
-        so we don't care anything > sum/2,
-        and by the Note above,
-        we can inspect that the max possible sum/2 is 100*200/2 = 10000,
-        since we still need place for sum equal to 0(the least significant bit),
-        so totally we need 10001 bits
-        
-        we initialize bits as 1,
-        this is saying that we can choose 0 elements to make up 0(x)
-        this is just for convenience, so that we can use bits |= bits << n;
-        */
-        bitset<10001> bits(1);
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        /*
-        initially, we can make up 0
-        1st iter: "bits" is set in 0th and nums[0]th bit
-        2nd iter: "bits" is set in 0, nums[0], nums[1] and nums[0]+nums[1]th bit
-        ...
-        */
-        for (auto n : nums) bits |= bits << n;
-        /*
-        need to check if the sum is even first!
-        bits[sum >> 1]: whether we can make up sum/2 by nums's subset
-        */
-        return !(sum & 1) && bits[sum >> 1];
+        int n=nums.size();
+        int sum=0;
+        //sum of array
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+        //if the sum of all ements of array is not even then it cannot be divided to equal sum subsets. 
+        if(sum%2!=0)
+            return false;
+        sum=sum/2; //we just look for the sum
+        int dp[n+1][sum+1];
+        //initialization
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=sum;j++)
+            {
+                if(i==0)
+                    dp[i][j]=false;
+                if(j==0)
+                    dp[i][j]=true;
+            }
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                if(nums[i-1]<=j)
+                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j]; //include it or leave it
+                else
+                    dp[i][j]=dp[i-1][j]; //leave it
+            }
+        }
+        return dp[n][sum];
     }
 };
